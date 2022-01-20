e9b4037c19495ac0640098479166bef3bfc506d1
Chubbs91 <chaskin1991@outlook.com>
2022-01-20 13:46:51 -0600

    updates to index.js - starting question array

--- Develop/index.js
+++ Develop/index.js
@@ -3,8 +3,25 @@ const fs = require('fs');
 
 console.log('hello node!');
 
-// TODO: Create an array of questions for user input
-// const questions = [];
+// questions for user input
+const questions = [
+    // Project Name/Title
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of the project? (Required)',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('Please enter a title to continue!');
+                return false;
+            }
+        }
+    },
+    // Project Description
+
+];
 
 // // TODO: Create a function to write README file
 // function writeToFile(fileName, data) {}
